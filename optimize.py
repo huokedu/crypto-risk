7a4ee97126a32091da9c1a3cccc9d7754acf7e71
ljeng <mailer.jeng@gmail.com>
2017-12-18 23:08:35 -0500

    Fixed evaluate for when f < 1. Fixed eff_front to work when n == 1.

--- optimize.py
+++ optimize.py
@@ -1,5 +1,6 @@
 import numpy, urllib.request, re, math, operator, functools
 from scipy import optimize
+r = float(re.findall('<td class=\"text_view_data\">(.+?)</td>', ''.join([x.decode('utf-8').strip() for x in urllib.request.urlopen('https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield')]))[-10])/100
 def variance(w, sigma, R):
 	product = numpy.multiply(w, sigma)
 	return numpy.matmul(numpy.matmul(numpy.transpose(product), R), product)
@@ -7,13 +8,15 @@ def eff_front(target, mu, sigma, R):
 	def con_mu(w, mu, target): return sum(numpy.multiply(w, mu))/sum(w) - target
 	n = len(mu)
 	assert n == len(sigma) == len(R)
-	weight = optimize.minimize(variance, len(mu)*[1/n], args = (sigma, R), bounds = n*[(0, 1)], constraints = ({'type': 'eq', 'fun': con_mu, 'args': (mu, target)}, {'type': 'eq', 'fun': lambda x: 1 - sum(x)}))
-	if weight.success: return weight.x, variance(weight.x, sigma, R)
+	if n != 1:
+		weight = optimize.minimize(variance, len(mu)*[1/n], args = (sigma, R), bounds = n*[(0, 1)], constraints = ({'type': 'eq', 'fun': con_mu, 'args': (mu, target)}, {'type': 'eq', 'fun': lambda x: 1 - sum(x)}))
+		if weight.success: return weight.x, variance(weight.x, sigma, R)
+	else: return [1], sigma[0]**2
 def kelly(target, mu, sigma, R, ef = None):
 	if not ef: ef = eff_front(target, mu, sigma, R)
 	return (sum(numpy.multiply(ef[0], mu)) - float(re.findall('<td class=\"text_view_data\">(.+?)</td>', ''.join([x.decode('utf-8').strip() for x in urllib.request.urlopen('https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield')]))[-10])/100)/ef[1]
 def kelly_opt(target, mu, sigma, R): return (kelly(target, mu, sigma, R) - 1)**2
 def evaluate(mu, sigma, R):
 	portfolio_mu = optimize.minimize(kelly_opt, min([(target, (kelly(target, mu, sigma, R) - 1)**2) for target in numpy.geomspace(min(mu), max(mu))], key = operator.itemgetter(1))[0], args = (mu, sigma, R), bounds = [(min(mu), max(mu))]).x
-	ef = eff_front(portfolio_mu, mu, sigma, R)
-	return {'weight': min(1, kelly(portfolio_mu, mu, sigma, R, ef))*(ef[0]), 'portfolio expected return': portfolio_mu[0], 'portfolio volatility': math.sqrt(ef[1])}
+	f, ef = min(1, kelly(portfolio_mu, mu, sigma, R, ef)), eff_front(portfolio_mu, mu, sigma, R)
+	return {'weight': [f*x for x in ef[0]], 'portfolio expected return': f*portfolio_mu[0], 'portfolio volatility': f*math.sqrt(ef[1])}
