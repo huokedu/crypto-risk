959c33bac5e201a76320ca12e2ebc5f716ca05c1
ljeng <mailer.jeng@gmail.com>
2017-12-21 21:00:55 -0500

    Added a function mu that computes the current expected return.

--- cape_mu.py
+++ cape_mu.py
@@ -1,4 +1,4 @@
-import urllib.request, pandas, csv, math, statistics, datetime, numpy
+import urllib.request, pandas, csv, math, statistics, datetime, numpy, re
 from scipy import stats
 ie_data = pandas.read_excel(urllib.request.urlopen('http://www.econ.yale.edu/~shiller/data/ie_data.xls'))
 ie = [list(ie_data[col]) for col in ie_data]
@@ -13,12 +13,16 @@ def write_pearsonr(file):
 				log_cape += [math.log(get_cape(i, t1_months))]
 				mu += [get_mu(i, t2, t2_months)]
 			w.writerow([t1, t2, stats.pearsonr(log_cape, mu)[0]])
-def lin_reg(t1, t2, file):
-	w = csv.writer(open(file, 'w', newline = ''))
+def log_reg(t1, t2, file = None):
+	if file: w = csv.writer(open(file, 'w', newline = ''))
 	t1_months, t2_months, cape_list, mu_list = 12*t1, 12*t2, [], []
 	for i in range(6 + t1_months, len(ie[0]) - t2_months - 1):
 		year, cape, mu = int(ie[0][i]), math.log(get_cape(i, t1_months)), get_mu(i, t2, t2_months)
 		cape_list += [cape]
 		mu_list += [mu]
-		w.writerow([datetime.datetime(year, int(100*(ie[0][i] - year) + 1/2), 1), cape, mu])
+		if file: w.writerow([datetime.datetime(year, int(100*(ie[0][i] - year) + 1/2), 1), cape, mu])
 	return numpy.polyfit(cape_list, mu_list, 1)
+def mu(t1, t2):
+	m, b = log_reg(t1, t2)
+	cape0 = get_cape(-2, 12*t1)
+	return m*math.log(get_cape(-2, 12*t1)) + b
