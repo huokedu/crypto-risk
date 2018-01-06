c2c458547fe30b759a03709b6c8b1df6f02361f2
ljeng <mailer.jeng@gmail.com>
2018-01-05 21:58:18 -0500

    Show miner fee in US$

--- miner_fee.py
+++ miner_fee.py
@@ -1,2 +1,7 @@
-import json
-json.loads(urllib.request.urlopen(urllib.request.Request('https://shapeshift.io/rate')).read().decode('utf-8'))
+import urllib.request, json
+def read_json(url): return json.loads(urllib.request.urlopen(urllib.request.Request(url)).read().decode('utf-8'))
+miner_fee, price, miner_fee_usd = {r['pair'].split('_')[1]: r['minerFee'] for r in read_json('https://shapeshift.io/rate')}, {cmc['symbol']: float(cmc['price_usd']) for cmc in read_json('https://api.coinmarketcap.com/v1/ticker/')}, {}
+i = 0
+for mf in miner_fee:
+	try: miner_fee_usd[mf] = miner_fee[mf]*price[mf]
+	except KeyError: continue
