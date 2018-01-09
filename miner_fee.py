0f8f73efd2e86ee7d2993437e26d99671326962c
ljeng <mailer.jeng@gmail.com>
2018-01-08 23:19:42 -0500

    Simplified miner fee calculation.

--- miner_fee.py
+++ miner_fee.py
@@ -1,6 +1,5 @@
 import urllib.request, json
-def read_json(url): return json.loads(urllib.request.urlopen(urllib.request.Request(url)).read().decode('utf-8'))
-data = read_json('https://shapeshift.io/rate')
+data = json.loads(urllib.request.urlopen(urllib.request.Request('https://shapeshift.io/rate')).read().decode('utf-8'))
 miner_fee = {x['pair'].split('_')[1]: x['minerFee'] for x in data}
 rate = {x['pair']: float(x['rate']) for x in data}
 mf_btc = {x: miner_fee[x]*rate[x + '_BTC'] for x in miner_fee if x != 'BTC'}
